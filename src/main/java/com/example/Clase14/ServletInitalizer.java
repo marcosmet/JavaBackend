ff6d1345b20849e72a81e0b78ced484a24b1787c
Marcosmet <marcosmet@gmail.com>
2025-05-19 15:18:00 -0300

    Agrego la entidad producto con su controlador

--- src/main/java/com/example/Clase14/ServletInitalizer.java
+++ src/main/java/com/example/Clase14/ServletInitalizer.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitalizer extends SpringBootServletInitializer {
 
     @Override
+
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Clase14Application.class);
     }
